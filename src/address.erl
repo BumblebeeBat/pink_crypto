3a77da155cae2ba5f06a4287ee65ec2ba46437c0
zack <zack.bitcoin@gmail.com>
2016-10-14 18:04:21 -0700

    made address more user friendly because order of inputs doesn't matter

--- src/address.erl
+++ src/address.erl
@@ -5,19 +5,19 @@
 
 pub2addr(L, M) ->
     pub2addr2(order(L), <<M:?weight_bits>>).
+order(L) -> %this is merge sort
+    M = column(L),%put everything into 1-lists.
+    order2(M).
 column([]) -> [];
 column([H|T]) -> [[H]|column(T)].
-order(L) ->
-    M = column(L),
-    order2(M).
-order2([X]) -> X;
+order2([X]) -> X; %keep doing it until there is only 1 left
 order2([H|T]) -> 
     order2(order3([H|T])).
-order3([A|[B|T]]) ->
+order3([A|[B|T]]) -> %merge pairs
     [merge(A, B)|order3(T)];
 order3([]) -> [];
 order3([X]) -> [X].
-merge([], X) -> X;
+merge([], X) -> X; %merge pair
 merge(X, []) -> X;
 merge([F1|B], [F2|D]) -> 
     {P1, _} = F1,
@@ -32,14 +32,15 @@ merge([F1|B], [F2|D]) ->
 pub2addr2([], B) -> hash:doit(B);
 pub2addr2([{Pub, Weight}|T], B) ->%This is slow for big addresses. 
     pub2addr2(T, <<Pub/binary, Weight:?weight_bits, B/binary>>).
-verify_signatures(Addr, PW, Min, Tx, L) ->
-    Addr = pub2addr(PW, Min),
-    verify2(PW, L, Tx, Min, 0).
+verify_signature(Addr, PW, Min, Tx, L) ->
+    PW2 = order(PW),
+    Addr = pub2addr2(PW2, <<Min:?weight_bits>>),
+    verify2(PW2, order(L), Tx, Min, 0).
 verify2([], [], _, A, B) when B =< A ->
     false;
 verify2([], [], _, A, B) when B > A ->
     true;
-verify2([{Pub, Weight}|T], [Signature|S], Tx, Min, Sum) ->
+verify2([{Pub, Weight}|T], [{Pub, Signature}|S], Tx, Min, Sum) ->
     if
 	Signature == 0 -> io:fwrite("zero\n");
 	true -> ok
@@ -56,9 +57,9 @@ verify2(_,_,_,_,_) ->
     
 test() ->
     {Pub1, Priv1} = sign:new_key(),
-    {Pub2, Priv2} = sign:new_key(),
+    {Pub2, _Priv2} = sign:new_key(),
     {Pub3, Priv3} = sign:new_key(),
-    {Pub4, Priv4} = sign:new_key(),
+    {Pub4, _Priv4} = sign:new_key(),
     A = {Pub1, 10},
     B = {Pub2, 5},
     C = {Pub3, 15},
@@ -69,8 +70,10 @@ test() ->
     Addr = pub2addr(PW, Min),
     Addr = pub2addr(PW2, Min),
     Tx = <<1,5,3,4>>,
-    L = [sign:sign(Tx, Priv1), <<>>, sign:sign(Tx, Priv3), <<>>],
-    true = verify_signatures(Addr, PW, Min, Tx, L),
+    L = [{Pub1, sign:sign(Tx, Priv1)}, {Pub3, sign:sign(Tx, Priv3)}, {Pub4, <<>>}, {Pub2, <<>>}],
+    true = verify_signature(Addr, PW, Min, Tx, L),
+    true = verify_signature(Addr, PW2, Min, Tx, L),
+    true = verify_signature(Addr, PW2, Min, Tx, lists:reverse(L)),
     success.
     
     
