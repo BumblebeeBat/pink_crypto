75357389f7a80484dface72c434a101832c4e98c
zack <zack.bitcoin@gmail.com>
2017-04-14 15:31:25 +0200

    fix hash function

--- src/hash.erl
+++ src/hash.erl
@@ -1,10 +1,17 @@
 -module(hash).
--export([doit/2]).
+-export([doit/2, bin_to_hex/1]).
 
 doit(S, Size) when not(is_binary(S)) -> 
     doit(term_to_binary(S), Size);
 doit(S, Size) -> 
     HD = Size * 8,
-    <<X:HD, _/bitstring>> = crypto:hmac(sha256, S, ""),
+    <<X:HD, _/bitstring>> = crypto:hash(sha256, S),%crypto:hmac(sha256, S, ""),
     <<X:HD>>.
+bin_to_hex(<<>>) -> "";
+bin_to_hex(<<A, B/binary>>) ->
+    byte_to_hex(<<A>>) ++ bin_to_hex(B).
+
+byte_to_hex(<< N1:4, N2:4 >>) ->
+    
+    [erlang:integer_to_list(N1, 16), erlang:integer_to_list(N2, 16)].
     
