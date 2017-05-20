723d11cefa2865a108c1bab50f7567ee692ec89c
zack <zack.bitcoin@gmail.com>
2017-05-20 08:53:23 -0700

    fix bug where if difficulty got to low it wouldn't work

--- install.sh
+++ install.sh
@@ -2,24 +2,18 @@
 if [ -e "rebar" ]
 then
     echo "rebar already installed"
-elif [ `uname -s`==Linux ]
-then
-    wget https://raw.githubusercontent.com/wiki/rebar/rebar/rebar && chmod u+x rebar
-elif [ `uname -s`==Darwin ]
-then
-    curl https://raw.githubusercontent.com/wiki/rebar/rebar/rebar -o rebar
-    chmod u+x rebar
-
 else
-    echo "your computer cannot compile this"
-fi
-
-#add erlang run time system to path
-PATH=$PATH:./rel/flying_fox/erts-6.4/bin
-
+    uname=$(uname);
+    case "$uname" in
+	(*Linux*) wget https://raw.githubusercontent.com/wiki/rebar/rebar/rebar && chmod u+x rebar; ;;
+	(*Darwin*) curl https://raw.githubusercontent.com/wiki/rebar/rebar/rebar -o rebar ;  chmod u+x rebar; ;;
+	(*) echo 'error: unsupported platform.'; exit 2; ;;
+    esac;
+    #$openCmd ;
 #use rebar to install other dependencies, explained in rebar.config
-./rebar get
-./rebar compile
+    ./rebar get
+    ./rebar compile
+fi
 
 sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
 
