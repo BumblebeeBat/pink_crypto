110cfa65dfa0da5328de8fef2e2d75370d29d345
zack <zack.bitcoin@gmail.com>
2017-06-01 09:26:10 -0600

    now we use the encryption repository instead of copy-pasting code

--- install.sh
+++ install.sh
@@ -1,27 +1,20 @@
-#first install rebar package manager
+#!/bin/bash
+# first install rebar package manager
+
 if [ -e "rebar" ]
 then
     echo "rebar already installed"
-elif [ `uname -s`==Linux ]
-then
-    wget https://raw.githubusercontent.com/wiki/rebar/rebar/rebar && chmod u+x rebar
-elif [ `uname -s`==Darwin ]
-then
-    curl https://raw.githubusercontent.com/wiki/rebar/rebar/rebar -o rebar
-    chmod u+x rebar
-
 else
-    echo "your computer cannot compile this"
+    uname=$(uname);
+    case "$uname" in
+	(*Linux*) wget https://raw.githubusercontent.com/wiki/rebar/rebar/rebar && chmod u+x rebar; ;;
+	(*Darwin*) curl https://raw.githubusercontent.com/wiki/rebar/rebar/rebar -o rebar ;  chmod u+x rebar; ;;
+	(*) echo 'error: unsupported platform.'; exit 2; ;;
+    esac;
+    #$openCmd ;
+    ./rebar get
+    ./rebar compile
 fi
 
-#add erlang run time system to path
-PATH=$PATH:./rel/flying_fox/erts-6.4/bin
 
-#use rebar to install other dependencies, explained in rebar.config
-./rebar get
-./rebar compile
-
-sh clean.sh #this deletes the database so every time we re-start, we have 0 blocks again. only needed during testing.
-
-echo "Successfully compiled Pink Hash".
 
