dce33e22627d36205f5b21bc71f92f09734996d6
zack <zack.bitcoin@gmail.com>
2017-11-15 07:51:20 -0800

    change how we serialize before hashing to make it easier to write javascript compatible code.

--- src/hash.erl
+++ src/hash.erl
@@ -4,7 +4,7 @@
 doit(S) ->
     doit(S, 32).
 doit(S, Size) when not(is_binary(S)) -> 
-    doit(term_to_binary(S), Size);
+    doit(sign:serialize(S), Size);
 doit(S, Size) -> 
     HD = Size * 8,
     <<X:HD, _/bitstring>> = crypto:hash(sha256, S),%crypto:hmac(sha256, S, ""),
@@ -12,8 +12,6 @@ doit(S, Size) ->
 bin_to_hex(<<>>) -> "";
 bin_to_hex(<<A, B/binary>>) ->
     byte_to_hex(<<A>>) ++ bin_to_hex(B).
-
 byte_to_hex(<< N1:4, N2:4 >>) ->
-    
     [erlang:integer_to_list(N1, 16), erlang:integer_to_list(N2, 16)].
     
