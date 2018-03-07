552183e2e9d1cbef38c48ab96085d3c9c24d651e
zack <zack.bitcoin@gmail.com>
2018-03-07 09:51:28 -0300

    now it can do both new and old versions

--- src/pow.erl
+++ src/pow.erl
@@ -1,36 +1,39 @@
 -module(pow).
 -export([data/1,pow/4,above_min/3,recalculate/3,
-	 sci2int/1,int2sci/1,nonce/1,check_pow/2,
-	 hash2integer/1,
+	 sci2int/1,int2sci/1,nonce/1,check_pow/3,
+	 hash2integer/2,
 	 test/0, test2/0]).
 -record(pow, {data, difficulty = 0, nonce}).
 nonce(P) -> P#pow.nonce.
 data(P) -> P#pow.data.
-above_min(P, Min, HashSize) ->
+above_min(P, Min, Fork) ->
     HashSize = 32,
-    true = check_pow(P, HashSize),
+    true = check_pow(P, HashSize, Fork),
     Diff = P#pow.difficulty,
     Diff >= Min.
-mining_check(P, HashSize) ->
-    check_common(P, HashSize, mining).
-check_pow(P, HashSize) ->
-    check_common(P, HashSize, verifying).
-check_common(P, HashSize, Type) ->
+mining_check(P, HashSize, Fork) ->
+    check_common(P, HashSize, Fork, mining).
+check_pow(P, HashSize, Fork) ->
+    check_common(P, HashSize, Fork, verifying).
+check_common(P, HashSize, Fork, Type) ->
     HashSize = 32,
     N = P#pow.nonce,
     Diff = P#pow.difficulty,
     Data = P#pow.data,
     H1 = hash:doit(Data, HashSize),
-    X = HashSize*8,
-    Y = <<N:X, H1/binary>>,%32 bytes of nonce followed by 32 bytes of the hash of the header.
+    X = HashSize*8, 
+    Y = case Fork of
+	    0 -> <<H1/binary, Diff:16, N:X>>;
+	    1 -> <<N:X, H1/binary>>%32 bytes of nonce followed by 32 bytes of the hash of the header.
+	end,
     H2 = hash:doit(Y, HashSize),
     I = case Type of
-	mining -> mining2integer(H2);
-	verifying -> hash2integer(H2)
+	mining -> mining2integer(H2, Fork);
+	verifying -> hash2integer(H2, Fork)
     end,
     I > Diff.
     
-pow(Data, Difficulty, Times, HashSize) ->
+pow(Data, Difficulty, Times, Fork) ->
     HashSize = 32,
     %bitcoin is 1,500,000 terahashes per second or 900,000,000,000,000,000,000 hashes per 10 minutes
     %in 10 years, bitcoin will find a collision of 88.6 bits. 
@@ -38,28 +41,51 @@ pow(Data, Difficulty, Times, HashSize) ->
     <<R:256>> = crypto:strong_rand_bytes(32),
     %T = math:pow(10,23),
     %R = round(random:uniform() * T),
-    pow2(Data, Difficulty, R, Times, HashSize).
-pow2(Data, Difficulty, Nonce, Times, HashSize) ->
+    pow2(Data, Difficulty, R, Times, HashSize, Fork).
+pow2(Data, Difficulty, Nonce, Times, HashSize, Fork) ->
     P = #pow{data = Data, difficulty = Difficulty, nonce = Nonce},
-    B = mining_check(P, HashSize),
+    B = mining_check(P, HashSize, Fork),
     if
 	Times < 1 -> false;
 	B -> P;
-	true -> pow2(Data, Difficulty, Nonce+1, Times-1, HashSize)
+	true -> pow2(Data, Difficulty, Nonce+1, Times-1, HashSize, Fork)
     end.
-mining2integer(H) -> mining2integer(<<H/binary, 255:8>>, 0).%identical to hash2integer. optimized for mining.
-mining2integer(<<0:1, B/bitstring>>, X) -> mining2integer(B, X+1);
-mining2integer(<<1:1, B:8, _/bitstring>>, X) -> pair2sci([X, B]).
-hash2integer(H) -> hash2integer(<<H/binary, 255:8>>, 0).%optimized for verification, not mining.
-hash2integer(<<0:128, T/bitstring>>, X) -> hash2integer(T, X+128);
-hash2integer(<<0:64, T/bitstring>>, X) -> hash2integer(T, X+64);
-hash2integer(<<0:32, T/bitstring>>, X) -> hash2integer(T, X+32);
-hash2integer(<<0:16, T/bitstring>>, X) -> hash2integer(T, X+16);
-hash2integer(<<0:8, T/bitstring>>, X) -> hash2integer(T, X+8);
-hash2integer(<<0:4, T/bitstring>>, X) -> hash2integer(T, X+4);
-hash2integer(<<0:2, T/bitstring>>, X) -> hash2integer(T, X+2);
-hash2integer(<<0:1, T/bitstring>>, X) -> hash2integer(T, X+1);
-hash2integer(<<1:1, B:8, _/bitstring>>, X) -> pair2sci([X, B]).
+mining2integer(H, Fork) -> mining2integer(<<H/binary, 255:8>>, 0, Fork).%identical to hash2integer. optimized for mining.
+mining2integer(<<0:1, B/bitstring>>, X, Fork) -> mining2integer(B, X+1, Fork);
+mining2integer(<<1:1, B:8, _/bitstring>>, X, Fork) -> 
+   B3 =  case Fork of
+	     0 -> 
+		 <<B2, _:1>> = <<1:1, B:8>>,
+		 B2;
+	     1 -> B
+	 end,
+    pair2sci([X, B3]).
+hash2integer(H, Fork) -> %optimized for verification, not mining.
+    hash2integer(<<H/binary, 255:8>>, 0, Fork).
+hash2integer(<<0:128, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+128, Fork);
+hash2integer(<<0:64, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+64, Fork);
+hash2integer(<<0:32, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+32, Fork);
+hash2integer(<<0:16, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+16, Fork);
+hash2integer(<<0:8, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+8, Fork);
+hash2integer(<<0:4, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+4, Fork);
+hash2integer(<<0:2, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+2, Fork);
+hash2integer(<<0:1, T/bitstring>>, X, Fork) -> 
+    hash2integer(T, X+1, Fork);
+hash2integer(<<1:1, B:8, _/bitstring>>, X, Fork) -> 
+   B3 =  case Fork of
+	     0 -> 
+		 <<B2, _:1>> = <<1:1, B:8>>,
+		 B2;
+	     1 -> B
+	 end,
+    pair2sci([X, B3]).
 exponent(_, 0) -> 1;
 exponent(A, 1) -> A;
 exponent(A, N) when N rem 2 == 0 -> 
@@ -104,13 +130,15 @@ recalculate(OldD, Top, Bottom) ->
     max(1, D).
     
 test() ->
+    S = success,
+    S = test(0),
+    S = test(1),
+    S.
+test(Fork) ->
     Data = <<5,2,6,0,10>>,
     Hash = hash:doit(Data, 32),
-    io:fwrite(integer_to_list(hash2integer(Hash))),
-    io:fwrite("\n"),
-    io:fwrite(integer_to_list(mining2integer(Hash))),
-    io:fwrite("\n"),
-    true = hash2integer(Hash) == mining2integer(Hash),
+    true = hash2integer(Hash, Fork) == 
+	mining2integer(Hash, Fork),
 
     HashSize = 32,
     D = 16,
@@ -120,14 +148,16 @@ test() ->
     Difficulty = pair2sci([D,D2]),
     %scientific notation, means we did about (2^(D-2))*(D2) hashes
     SearchTimes = round(math:pow(2, D)),
-    case pow(Data, Difficulty, SearchTimes, HashSize) of
+    case pow(Data, Difficulty, SearchTimes, Fork) of
 	false -> io:fwrite("failed to find a block quick enough\n");
 	P ->
-	    io:fwrite("found a block\n"),
-	    true = above_min(P, Difficulty, HashSize),
-	    false = above_min(P, Difficulty+1, HashSize),
-	    true = check_pow(P, HashSize),
-	    true = mining_check(P, HashSize),
+	    io:fwrite("found a block on fork "),
+	    io:fwrite(integer_to_list(Fork)),
+	    io:fwrite("\n"),
+	    true = above_min(P, Difficulty, Fork),
+	    false = above_min(P, Difficulty+1, Fork),
+	    true = check_pow(P, HashSize, Fork),
+	    true = mining_check(P, HashSize, Fork),
 	    P
     end,
     success.
@@ -136,6 +166,6 @@ test2() ->
     A = <<0, 1, 2, 255, 255, 255, 255, 255>>,
 %0x007FFFFFFFFFFFFFFFFFFFFFFFFFFFFF
     B = <<0, 111, 255, 255, 255, 255, 255>>,
-    [hash2integer(A),
-     hash2integer(B)] == [3969,2527].%returns true.
+    [hash2integer(A, 0),
+     hash2integer(B, 0)] == [3969,2527].%returns true.
 	       
